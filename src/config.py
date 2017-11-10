b467dde2ec3e698ab27fcb63830f0d57d2e407ce
Keivaun Waugh <keivaunwaugh@gmail.com>
2017-11-10 13:16:08 -0600

    read in the data.

--- src/config.py
+++ src/config.py
@@ -2,6 +2,12 @@ import os
 import scipy.io
 import numpy as np
 import utils
+import pickle
+
+def unpickle(file):
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo, encoding='bytes')
+    return dict
 
 # for resnet
 input_shape = [224, 224, 3]
@@ -15,20 +21,21 @@ batch_size = 256
 model_name = 'ResNet56'
 log_dir = 'log/'
 
-data_dir = '../data/'
-filenames = scipy.io.loadmat(os.path.join(data_dir, 'filenames.mat'))
-classnames = filenames['classnames']
-trainImNames = filenames['trainImNames']
-test1ImNames = filenames['test1ImNames']
-
-test2ImNames = scipy.io.loadmat(os.path.join(data_dir, 'test2ImNames.mat'))
-test2ImNames = test2ImNames['test2ImNames']
-
-extraTrainImNames = filenames['extraTrainImNames']
-# concatenate trainImNames and extraTrainImNames
-allTrainImNames = np.column_stack((trainImNames, extraTrainImNames))
-
-class_to_label = utils.get_class_to_label()
-label_to_class = utils.get_label_to_class()
-
-num_classes = 10
+data_dir = '../data/cifar-10-batches-py/'
+data_batch_names = ['data_batch_{0}'.format(i) for i in range(1, 6)]
+# A len(data_batch_names)x10000x3072 numpy array of uint8s.
+# Each row of the array stores a 32x32 colour image.
+# The first 1024 entries contain the red channel values, the next 1024 the
+# green, and the final 1024 the blue.
+# The image is stored in row-major order, so that the first 32 entries of the
+# array are the red channel values of the first row of the image.
+data = []
+# A list of len(data_batch_names)x10000 numbers in the range 0-9.
+# The number at index i indicates the label of the ith image in the array data.
+labels = []
+for i in range(len(data_batch_names)):
+    data_batches = unpickle(os.path.join(data_dir, data_batch_names[i]))
+    data.append(data_batches[b'data'])
+    labels.append(data_batches[b'labels'])
+# data_batches = list(map(lambda name: unpickle(os.path.join(data_dir, name)),
+        # data_batch_names))
